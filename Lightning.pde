a793bc09d07490f56b7c885ea1a0e4a4b446faf7
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-09-04 11:28:46 -0700

    Added lightning and rain.

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,37 @@
+
 void setup()
 {
-  size(300,300);
-}
-void draw()
-{
-
+	size(500,500);
+	strokeWeight(0.5);
 }
 void mousePressed()
 {
+	int startX = mouseX;
+	int startY = 0;
+	int endX = startX + (int)(Math.random() * 10 - 5);
+	int endY = startY + (int)(Math.random() * 5);
+	while(startX < 500)
+	{
+		stroke(startX/2, 100, 150);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+		endX = endX + (int)(Math.random() * 10 - 5);
+		endY = endY + (int)(Math.random() * 5);
+	}
+}
+void draw()
+{
+	//rain
+	frameRate(8);
+	background(50);
+	stroke(132, 172, 225, 50);
+	for(int y = 0; y<500; y = y + 20)
+	{
+		for(int x = 0; x<500; x= x + 10)
+		{
+			line(x, y, x, y+(int)(Math.random() * 10));
+		}
+	}
 
 }
-
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,64 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
-}
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	size(500,500);
+	strokeWeight(0.5f);
+
 }
 public void mousePressed()
 {
-  redraw();
+	int startX = mouseX;
+	int startY = 0;
+	int endX = startX + (int)(Math.random() * 10 - 5);
+	int endY = startY + (int)(Math.random() * 5);
+	while(startX < 500)
+	{
+		stroke(startX/2, 100, 150);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+		endX = endX + (int)(Math.random() * 10 - 5);
+		endY = endY + (int)(Math.random() * 5);
+	}
 }
+public void draw()
+{
+	frameRate(8);
+	background(50);
+	stroke(132, 172, 225, 50);
+	for(int y = 0; y<500; y = y + 20)
+	{
+		for(int x = 0; x<500; x= x + 10)
+		{
+			line(x, y, x, y+(int)(Math.random() * 10));
+			/*if(y > 380)
+			{
+				y = 0;
+			}
+			System.out.println(x + "and" + y);*/
+		}
+	}
 
+}
+/*void mousePressed()
+{
+	int startX = mouseX;
+	int startY = 0;
+	int endX = startX + (int)(Math.random() * 10 - 5);
+	int endY = startY + (int)(Math.random() * 5);
+	while(startX < 500)
+	{
+		stroke(startX/2, 150, 150);
+		line(startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+		endX = endX + (int)(Math.random() * 10 - 5);
+		endY = endY + (int)(Math.random() * 5);
+	}
+}*/
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
